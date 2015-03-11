b0448cdf5810bb37dfe562e28466f52bb81062b5
mads jensen <mje.mads@gmail.com>
2015-03-11 21:21:11 +0100

    small update of path

--- preprocess_data.R
+++ preprocess_data.R
@@ -1,3 +1,13 @@
 # setwd("/home/mje/Projects/MEG_RP_behav")
 
 setwd("/home/mje/Projects/MEG_RP_behav/Int_binding/data")
+
+
+ldf <- list() # creates a list
+listcsv <- dir(pattern = "*.csv") # creates the list of all the csv files in the directory
+
+for (k in 1:length(listcsv)){
+    ldf[[k]] <- read.csv(listcsv[k], header = FALSE)
+}
+
+)
